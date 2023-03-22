b13b438e77577776ef22097ecd09df4f7ce9c9a1
Etinosa Izekor <izekoretinosa15@gmail.com>
2023-03-22 17:33:20 +0200

    Add login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,16 +4,41 @@ import Container from '../../components/Container';
 import Form from 'react-bootstrap/Form';
 
 const LoginFormContainer = styled(Container)`
-  width: 30vw;
-  height: 50vh;
+  width:50vw;
+  height: 70vh;
+  align-items: center;
+  background-color: ;
+  padding: 70px;
+  position: absolute;
+  margin: 10px 120px;
+  display: flex;
+  gap: 100px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.15),  0 -1px 2px rgba(0,0,0,0.09);
+
+  &:hover{
+    box-shadow: 0 4px 8px rgba(0,0,0,0.09);
+  }
+`
+const ImageContainer = styled.div`
+  width: 400px;
+  height: 400px;
+  overflow: hidden;
   align-items: center;
   justify-content: center;
-  margin: auto;
+`
+
+const Image = styled.img`
+  width: 100%;
+  height:100%;
+  object-fit: cover;
 `
 
 function Login() {
   return (
     <LoginFormContainer>
+      <ImageContainer>
+        <Image src={require('../../assets/study.png')} alt="" />
+      </ImageContainer>
       <Form>
         <h3 style={{paddingBottom: "10px"}}>Login</h3>
         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -26,9 +51,8 @@ function Login() {
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <Button variant="secondary" type="submit">
+        <Button variant="secondary" type="submit" style={{width: "100%", marginTop: "10px"}}>
           Submit
         </Button>
       </Form>
